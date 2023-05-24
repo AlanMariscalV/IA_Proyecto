e635a0eab74ffce08801155be470d2d1c9d73199
AlanMariscalV <129803240+AlanMariscalV@users.noreply.github.com>
2023-05-24 08:47:52 -0600

    b

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ def apriori2(data,soporte,elevacion,confianza):
                    min_confidence=confianza, 
                    min_lift=elevacion)
     ResultadosC1 = list(ReglasC1)
-    pd.DataFrame(ResultadosC1)
-    for item in ResultadosC1:
+    Rc1=pd.DataFrame(ResultadosC1)
+    #for item in ResultadosC1:
         #El primer índice de la lista
         #Emparejar = item[0]
         #items = [x for x in Emparejar]
@@ -52,7 +52,7 @@ def apriori2(data,soporte,elevacion,confianza):
         #El tercer índice de la lista
         #st.write("Confianza: " + str(item[2][0][2]))
         #st.write("Elevación: " + str(item[2][0][3])) 
-        st.write(ResultadosC1 + "=====================================") # Aquí deberíamos asociar el valor correspondiente a la etiqueta
+    st.write(Rc1 + "=====================================") # Aquí deberíamos asociar el valor correspondiente a la etiqueta
 
 
 # Opciones de navegación y contenido de las páginas
