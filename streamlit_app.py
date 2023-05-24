bde322800c1f68bba980365d2ead79a31e507d0e
AlanMariscalV <129803240+AlanMariscalV@users.noreply.github.com>
2023-05-23 23:20:08 -0600

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
-st.title("intento imagen")
 
-uploaded_file=st.file_uploader("selecciona",type=["jpg","jpeg","png"])
+# Título de la página
+st.title("Carga de imágenes en Streamlit")
 
+# Componente para cargar la imagen
+uploaded_file = st.file_uploader("Selecciona una imagen", type=["jpg", "jpeg", "png"])
+
+# Verificar si se cargó un archivo
 if uploaded_file is not None:
-
-    st.image(uploaded_file,caption="imagen cargada",use_colum_width=True)
+    # Mostrar la imagen cargada
+    st.image(uploaded_file, caption="Imagen cargada", use_column_width=True)
