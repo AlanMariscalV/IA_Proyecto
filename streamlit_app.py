2ac6de958705273f7ad8149b23f085022d2770a9
AlanMariscalV <129803240+AlanMariscalV@users.noreply.github.com>
2023-05-23 23:09:36 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit as st
+st.title("intento imagen")
 
+uploaded_file=st.file_uploader("delecciona",type=["jpg","jpeg","png"])
+
+if uploaded_file is not None:
+
+    st.image(uploaded_file,caption="imagen cargada",use_colum_width=True)
