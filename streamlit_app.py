b558a2c22b7948479ebb9c29b1393c612c0610b0
AlanMariscalV <129803240+AlanMariscalV@users.noreply.github.com>
2023-05-24 20:50:24 -0600

    alfa

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ def metricas(dato,metrica_seleccionada,lambda1 = 0):
     st.write(Matriz.round(3))
     return Matriz
     
-def sacarDistancia(metrica,MEstandarizada,lamda2 = 0):
+def sacarDistancia(metrica,MEstandarizada,lambda2 = 0):
         options = ['0','1', '2', '3','4', '5', '6','7', '8', '9']
         selected_options = st.multiselect('Selecciona dos objetos para sacar la distancia:', options)
         if len(selected_options) == 2:
@@ -89,7 +89,7 @@ def sacarDistancia(metrica,MEstandarizada,lamda2 = 0):
             elif metrica == 3: 
                 dst = distance.chebyshev(Objeto1,Objeto2)
             elif metrica == 4: 
-                dst = distance.minkowski(Objeto1,Objeto2,lamda2)
+                dst = distance.minkowski(Objeto1,Objeto2,lambda2)
             st.write("La distancia entre los objetos es: ", dst)
         elif len(selected_options) > 2:
             st.write('¡Has seleccionado más de dos opciones! Selecciona solo dos.')
